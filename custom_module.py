2f1ab0a1ed16df389d3ee6e03c385b05cc4e8b4b
cuateramiro <132035985+cuateramiro@users.noreply.github.com>
2025-01-18 12:28:26 -0800

    Delete custom_module.py

--- custom_module.py
+++
@@ -1,4 +0,0 @@
-# Defining a function that incorporates year, destination, and cost to create a message about the time travel experience:
-
-def generate_time_travel_message(target_year, destination, cost):
-  print(f"Hey, there! Your destination to the {destination} year {target_year} will cost you {cost}")
